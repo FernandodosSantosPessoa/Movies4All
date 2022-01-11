33f7e323475d87da8ace1cd161e6d6a3e1381caa
Fernando Pessoa <fernandodspessoa@hotmail.com>
2022-01-11 20:47:00 -0300

    Final version of the lists with all the correction needed (inlcueded inflater)

--- app/src/main/java/com/example/movies4all/ui/HomeFragment.kt
+++ app/src/main/java/com/example/movies4all/ui/HomeFragment.kt
@@ -5,38 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movies4all.R
+import com.example.movies4all.databinding.FragmentHomeBinding
 import com.example.movies4all.viewmodel.HomeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
 
-
     val viewModel:HomeViewModel by viewModel()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        viewModel.listOfMovies?.observe(viewLifecycleOwner, Observer {
-            makeToast()
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false
+        )
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        listsOfMoviesObserver()
+
+        return binding.root
+    }
+
+    private fun listsOfMoviesObserver() {
+        viewModel.listOfMovies?.observe(viewLifecycleOwner, Observer { lists ->
+            lists?.let {
+                binding.rvListsOfMovies.layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+                binding.rvListsOfMovies.adapter = ListsOfMoviesAdapter(requireContext(), lists)
+            }
         })
-
-        return view
     }
-
-
-
-
-
-
-
-    private fun makeToast() {
-        Toast.makeText(context, "Fernando é gay!", Toast.LENGTH_LONG).show()
-    }
-
 }
